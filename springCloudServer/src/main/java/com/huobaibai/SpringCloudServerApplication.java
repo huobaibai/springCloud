f9cca923e942241c9e6f2f48f83478cd50e03ccb
Jian.Yang <Jian.Yang@CN2210436W1.ey.net>
2017-08-28 15:35:08 +0800

    springcloudServer

---
+++ springCloudServer/src/main/java/com/huobaibai/SpringCloudServerApplication.java
@@ -0,0 +1,16 @@
+package com.huobaibai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringCloudServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudServerApplication.class, args);
+		System.out.println("11");
+	}
+}
